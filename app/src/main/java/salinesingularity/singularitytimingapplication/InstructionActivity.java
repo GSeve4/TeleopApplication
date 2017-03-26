ca69a1120c68cbc8e19582d21394cc6f6cd765ce
Seve4 <gseve08@gmail.com>
2017-03-26 16:12:31 -0400

    Update Instruction Page

--- app/src/main/java/salinesingularity/singularitytimingapplication/InstructionActivity.java
+++ app/src/main/java/salinesingularity/singularitytimingapplication/InstructionActivity.java
@@ -1,13 +1,28 @@
 package salinesingularity.singularitytimingapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InstructionActivity extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+        backButton = (Button) findViewById(R.id.btnInst);
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(InstructionActivity.this,StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
